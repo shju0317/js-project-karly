6b70dcbad5a39a37cb3d296f11a5d673b1c633c6
shju0317 <shju0317@naver.com>
2023-07-31 22:03:54 +0900

    🎨Feat: productDetail 기능 구현

--- client/js/productList.js
+++ client/js/productList.js
@@ -1,4 +1,4 @@
-import { getNode, renderProduct, renderProductQuantity, tiger } from '../lib/index.js';
+import { bindEvent, getNode, renderProduct, renderProductQuantity,  selectedProduct,  tiger } from '../lib/index.js';
 
 /* 베스트 상품 목록 렌더링 */
 const productList = getNode('.productList');
@@ -11,3 +11,44 @@ itemList.forEach((item) => renderProduct(productList, item))
 /* 상품 수량 렌더링 (총 n건) */
 renderProductQuantity(itemList);
 
+
+/* 상품 상세정보 렌더링 */
+const productDetail = getNode('.productDetail');
+// console.log(productDetail);
+
+/* 선택한 상품 id 불러오기 */
+// selectedProduct(productList);
+
+// let productID = '';
+// bindEvent(productList,'click',(item)=>{
+//   const figure = item.target.closest('figure');
+//   productID = figure.querySelector('figcaption > span').textContent;
+//   console.log(productID);
+//   return productID;  
+//   })
+
+// let productID = '';
+// function test(){
+//   bindEvent(productList,'click',(item)=>{
+//     const figure = item.target.closest('figure');
+//     productID = figure.querySelector('figcaption > span').textContent;
+//     console.log('1: '+productID);
+//     return productID;  
+//   })
+//   console.log('2: '+productID);
+//   return productID;
+// }
+// test();
+
+// console.log('여기: '+productID);
+
+let productID = bindEvent(productList,'click',(item)=>{
+    const figure = item.target.closest('figure');
+    productID = figure.querySelector('figcaption > span').textContent;
+    console.log('1: '+productID);
+    return productID;  
+  })
+  // console.log('2: '+productID);
+  // return productID;
+
+console.log('여기: '+productID);
