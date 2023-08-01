8e4a74f73ea5bcf195f18ed9c12796e51f2831f9
GwonH <kk.filament@gmail.com>
2023-08-01 16:35:50 +0900

    🛠modify : 메인 헤더영역, 장바구니 모달창 text size 수정
    한 단계씩 키웠습니다.(xs -> sm)

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,7 @@
 import {  addClass, attr, removeClass, getNode, renderItemList, } from '../lib/index.js';
 
 
+
 renderItemList();
 
 
@@ -10,6 +11,7 @@ const swiperWrapper2 = getNode('.swiperWrapper2')
 const swiperWrapper3 = getNode('.swiperWrapper3')
 
 let totalCart = 0;
+let index = 0;
 
 /* 모달 열릴때마다 초기화 확인 */
 let isModalInitialized = false;
@@ -21,15 +23,14 @@ function handleAddCartModal(e) {
   const modalClose = getNode('.addCartModal__button__modalClose');
   const modalAdd = getNode('.addCartModal__button__modalAdd');
 
+
   // 클릭한 대상의 data-index값 불러오기
-  
+
   const cartIndex = e.target.closest('button')
-  const index = attr(cartIndex,'data-index');
+  index = attr(cartIndex,'data-index');
   console.log(index);
 
-
-
-
+  fetchData();
   if(button){
     if (!isModalInitialized) {
       modalClose.addEventListener('click', () => {
@@ -57,19 +58,43 @@ swiperWrapper3.addEventListener('click', handleAddCartModal)
 
 
 
-// // data.json 불러오기
-function loadData(index){
 
-  fetch('/server/db/data.json')
-    .then(response => response.json())
-    .then(data => {
-      const clickedItem = data.find(item => item.id === index)
-      console.log(clickedItem);
-    })
-    .catch(refError)
+  
+
+/* 모달창에 data 띄우기 */
+  const productCount = getNode('.productCount')
+  let productCountTextContent = 1;
+
+
+  async function fetchData() {
+    try {
+      const productName = getNode('.addCartModal__productName ')
+      const productCost = getNode('.modalProduct')
+      const productTotal = getNode('.addCartModal__total__cost')
+      const response = await fetch('http://localhost:3000/products');
+      const jsondata = await response.json();
+
+      productName.textContent = jsondata[index-1].name
+      productCost.textContent = jsondata[index-1].price + `원`
+      console.log();
+      productTotal.textContent = (jsondata[index-1].price)*+productCountTextContent  + `원`
+
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+
+
+
+const addProduct = getNode('.addProduct')
+
+function handleModalCartCountUp(){
+  // console.log("어이");
+  ++productCountTextContent;
+  
+  productCount.textContent = productCountTextContent;
 }
 
-
-
-
-// handleProductInfo()
+addProduct.addEventListener('click',handleModalCartCountUp)
